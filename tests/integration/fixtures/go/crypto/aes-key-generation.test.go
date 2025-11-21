ec9e3cf3506756b7c30ef192eafda8d0802b0d61
Matias Daloia <66310421+matiasdaloia@users.noreply.github.com>
2025-11-21 09:54:28 +0000

    feat: add testing framework (#11)
    * feat: add testing framework
    
    * feat: add crypto-finder as submodule

---
+++ tests/integration/fixtures/go/crypto/aes-key-generation.test.go
@@ -0,0 +1,18 @@
+// TEST-RULE: go.crypto.aes.key-generation
+// TEST-METADATA: algorithmName=AES, library=crypto/aes, api=aes.NewCipher, parameterSetIdentifier=32
+
+package main
+
+import (
+	"crypto/aes"
+)
+
+func main() {
+	// Test AES key generation with 32-byte key (AES-256)
+	key := make([]byte, 32)
+	cipher, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+	_ = cipher
+}
