87fd79a84d77551eb6b739f0cc1fd2a4c457e9e4
Shaun Smith <51304449+smith-xyz@users.noreply.github.com>
2025-12-12 16:31:38 -0500

    feat(go): reduce duplication of crypto md5 and sha rules

---
+++ tests/integration/fixtures/go/crypto/algorithm/sha/sha1.test.go
@@ -0,0 +1,32 @@
+package sha_test
+
+import (
+	"crypto"
+	"crypto/sha1"
+)
+
+// TEST-RULE: go.crypto.sha1.hash-usage
+// TEST-METADATA: operation:digest findingType:hash algorithmName:SHA-1 api:sha1.New
+func testSHA1New() {
+	// ruleid: go.crypto.sha1.hash-usage
+	hasher := sha1.New()
+	_ = hasher
+}
+
+// TEST-RULE: go.crypto.sha1.hash-usage
+// TEST-METADATA: operation:digest findingType:hash algorithmName:SHA-1 api:sha1.Sum
+func testSHA1Sum() {
+	data := []byte("test message")
+	// ruleid: go.crypto.sha1.hash-usage
+	hash := sha1.Sum(data)
+	_ = hash
+}
+
+// TEST-RULE: go.crypto.sha1.hash-usage
+// TEST-METADATA: operation:digest findingType:hash algorithmName:SHA-1 api:crypto.SHA1
+func testSHA1CryptoConst() {
+	// ruleid: go.crypto.sha1.hash-usage
+	hasher := crypto.SHA1.New()
+	_ = hasher
+}
+
